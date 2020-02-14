c0031978d6cbde1a9d5739bb9aee396a6a6aa1c2
applichic <gbelouin@applichic.com>
2020-02-14 12:23:01 +0100

    Add chart and colorify it.

--- lib/ui/screens/chart_screen.dart
+++ lib/ui/screens/chart_screen.dart
@@ -3,14 +3,45 @@ import 'package:chic_wallet/providers/theme_provider.dart';
 import 'package:chic_wallet/ui/components/bank_body.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class ChartScreen extends StatefulWidget {
   @override
   _ChartScreenState createState() => _ChartScreenState();
 }
 
-class _ChartScreenState extends State<ChartScreen> with AutomaticKeepAliveClientMixin<ChartScreen> {
+class _ChartScreenState extends State<ChartScreen>
+    with AutomaticKeepAliveClientMixin<ChartScreen> {
   ThemeProvider _themeProvider;
+
+  List<charts.Series> _seriesList;
+
+  @override
+  void initState() {
+    _loadChartsData();
+    super.initState();
+  }
+
+  _loadChartsData() {
+    final data = [
+      LinearTransactions(0, 100.0, charts.Color.fromHex(code: "#fe3523")),
+      LinearTransactions(1, 75.0, charts.Color.fromHex(code: "#2a828f")),
+      LinearTransactions(2, 25.0, charts.Color.fromHex(code: "#29514c")),
+      LinearTransactions(3, 5.0, charts.Color.fromHex(code: "#2a4f8f")),
+    ];
+
+    var series = charts.Series<LinearTransactions, int>(
+      id: 'Transactions',
+      domainFn: (LinearTransactions sales, _) => sales.category,
+      measureFn: (LinearTransactions sales, _) => sales.price,
+      data: data,
+      colorFn: (LinearTransactions sales, _) => sales.color,
+    );
+
+    setState(() {
+      _seriesList = [series];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,10 +59,34 @@ class _ChartScreenState extends State<ChartScreen> with AutomaticKeepAliveClient
           ),
         ),
       ),
-      child: Container(),
+      child: Container(
+        margin: EdgeInsets.only(top: 330, left: 16, right: 16),
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Container(
+                height: 300,
+                child: charts.PieChart(
+                  _seriesList,
+                  animate: true,
+                  defaultRenderer: charts.ArcRendererConfig(arcWidth: 98),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
   @override
   bool get wantKeepAlive => true;
+}
+
+class LinearTransactions {
+  final int category;
+  final double price;
+  final charts.Color color;
+
+  LinearTransactions(this.category, this.price, this.color);
 }
