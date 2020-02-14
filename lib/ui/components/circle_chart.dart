96adcc9a2a2d5fdc5c80e405ecf2897515b9ca23
applichic <gbelouin@applichic.com>
2020-02-14 14:41:24 +0100

    Displays the chart corresponding to the transactions of the selected bank.

---
+++ lib/ui/components/circle_chart.dart
@@ -0,0 +1,109 @@
+import 'package:chic_wallet/localization/app_translations.dart';
+import 'package:chic_wallet/providers/bank_provider.dart';
+import 'package:chic_wallet/providers/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:provider/provider.dart';
+
+class CircleChart extends StatefulWidget {
+  @override
+  _CircleChartState createState() => _CircleChartState();
+}
+
+class _CircleChartState extends State<CircleChart> {
+  ThemeProvider _themeProvider;
+  List<charts.Series> _seriesList;
+  BankProvider _bankProvider;
+
+  _loadChartsData() {
+    List<LinearTransactions> data = [];
+
+    for (var transaction in _bankProvider.transactions) {
+      var transactionTypeData = data
+          .where((d) => d.category == transaction.typeTransaction.id)
+          .toList();
+
+      if (transactionTypeData.isEmpty) {
+        data.add(LinearTransactions(
+          transaction.typeTransaction.id,
+          transaction.price,
+          charts.Color.fromHex(code: "#fe3523"),
+        ));
+      } else {
+        transactionTypeData[0].price += transaction.price;
+      }
+    }
+
+    var series = charts.Series<LinearTransactions, int>(
+      id: 'Transactions',
+      domainFn: (LinearTransactions sales, _) => sales.category,
+      measureFn: (LinearTransactions sales, _) => sales.price,
+      data: data,
+      colorFn: (LinearTransactions sales, _) => sales.color,
+    );
+
+    if (data.isEmpty) {
+      setState(() {
+        _seriesList = [];
+      });
+    } else {
+      setState(() {
+        _seriesList = [series];
+      });
+    }
+  }
+
+  Widget _displaysChart() {
+    return Container(
+      height: 300,
+      child: charts.PieChart(
+        _seriesList,
+        animate: true,
+        defaultRenderer: charts.ArcRendererConfig(arcWidth: 98),
+      ),
+    );
+  }
+
+  Widget _displaysEmptyData() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16),
+      child: Column(
+        children: <Widget>[
+          Image(
+            image: AssetImage('assets/empty.png'),
+            color: _themeProvider.textColor,
+          ),
+          Text(
+            AppTranslations.of(context).text("home_screen_no_transactions"),
+            style: TextStyle(
+              color: _themeProvider.textColor,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _themeProvider = Provider.of<ThemeProvider>(context, listen: true);
+    _bankProvider = Provider.of<BankProvider>(context, listen: true);
+    _loadChartsData();
+
+    if (_seriesList.isEmpty) {
+      return _displaysEmptyData();
+    } else {
+      return _displaysChart();
+    }
+  }
+}
+
+class LinearTransactions {
+  final int category;
+  double price;
+  final charts.Color color;
+
+  LinearTransactions(this.category, this.price, this.color);
+}
--- lib/ui/screens/chart_screen.dart
+++ lib/ui/screens/chart_screen.dart
@@ -1,9 +1,9 @@
 import 'package:chic_wallet/localization/app_translations.dart';
 import 'package:chic_wallet/providers/theme_provider.dart';
 import 'package:chic_wallet/ui/components/bank_body.dart';
+import 'package:chic_wallet/ui/components/circle_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
 
 class ChartScreen extends StatefulWidget {
   @override
@@ -13,35 +13,6 @@ class ChartScreen extends StatefulWidget {
 class _ChartScreenState extends State<ChartScreen>
     with AutomaticKeepAliveClientMixin<ChartScreen> {
   ThemeProvider _themeProvider;
-
-  List<charts.Series> _seriesList;
-
-  @override
-  void initState() {
-    _loadChartsData();
-    super.initState();
-  }
-
-  _loadChartsData() {
-    final data = [
-      LinearTransactions(0, 100.0, charts.Color.fromHex(code: "#fe3523")),
-      LinearTransactions(1, 75.0, charts.Color.fromHex(code: "#2a828f")),
-      LinearTransactions(2, 25.0, charts.Color.fromHex(code: "#29514c")),
-      LinearTransactions(3, 5.0, charts.Color.fromHex(code: "#2a4f8f")),
-    ];
-
-    var series = charts.Series<LinearTransactions, int>(
-      id: 'Transactions',
-      domainFn: (LinearTransactions sales, _) => sales.category,
-      measureFn: (LinearTransactions sales, _) => sales.price,
-      data: data,
-      colorFn: (LinearTransactions sales, _) => sales.color,
-    );
-
-    setState(() {
-      _seriesList = [series];
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,14 +35,7 @@ class _ChartScreenState extends State<ChartScreen>
         child: Column(
           children: <Widget>[
             Center(
-              child: Container(
-                height: 300,
-                child: charts.PieChart(
-                  _seriesList,
-                  animate: true,
-                  defaultRenderer: charts.ArcRendererConfig(arcWidth: 98),
-                ),
-              ),
+              child: CircleChart(),
             ),
           ],
         ),
@@ -81,12 +45,4 @@ class _ChartScreenState extends State<ChartScreen>
 
   @override
   bool get wantKeepAlive => true;
-}
-
-class LinearTransactions {
-  final int category;
-  final double price;
-  final charts.Color color;
-
-  LinearTransactions(this.category, this.price, this.color);
 }
